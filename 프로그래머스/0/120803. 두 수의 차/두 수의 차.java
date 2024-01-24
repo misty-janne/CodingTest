7251eb6d1a4afa20cde1ca0c65043f1e98092460
misty-janne <62362285+misty-janne@users.noreply.github.com>
2024-01-24 19:31:11 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        if (num1 < -50000 && num1 > 50000
+           && num2 < -50000 && num1 > 50000) return 0;
+        
+        return num1 - num2;
+    }
+}
