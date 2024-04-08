6a214c6f37d397bbfd5fd145755226b529038f02
hy <62362285+misty-janne@users.noreply.github.com>
2024-04-08 16:15:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -3,17 +3,11 @@ import java.lang.Math;
 class Solution {
     public int solution(int price) {
         int answer = 0;
-        
-        if (price >= 500000) {
-            answer = (int) ((double)price - price*0.2);
-        } else if (price >= 300000) {
-            answer = (int) ((double)price - price*0.1);
-        } else if (price >= 100000) {
-            answer = (int) ((double)price - price*0.05);
-        } else {
-            answer = price;
-        }
 
-        return answer;
+        if(price>=500000) return (int)(price*0.8);
+        if(price>=300000) return (int)(price*0.9);
+        if(price>=100000) return (int)(price*0.95);
+
+        return price;
     }
 }
