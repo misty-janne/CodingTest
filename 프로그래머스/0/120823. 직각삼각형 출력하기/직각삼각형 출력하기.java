bc9948c3176d8a65df19f895570e132fdc494aa4
hy <62362285+misty-janne@users.noreply.github.com>
2024-05-02 18:32:52 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 233.99 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++){
+            sb.append("*");
+            System.out.println(sb);
+        }
+        sc.close();
+    }
+}
