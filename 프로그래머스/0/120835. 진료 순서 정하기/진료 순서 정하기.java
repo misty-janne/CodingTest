28bc9c975bee6f469cdcb8993695bc04778d79b7
hy <62362285+misty-janne@users.noreply.github.com>
2024-05-30 19:41:30 +0900

    Update 진료 순서 정하기.java

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -5,16 +5,11 @@ class Solution {
         int[] oriArr = Arrays.copyOf(emergency, emergency.length);
 
         Arrays.sort(emergency);
-        for (int i = 0; i < emergency.length / 2; i++) {
-            int temp = emergency[i];
-            emergency[i] = emergency[emergency.length - 1 - i];
-            emergency[emergency.length - 1 - i] = temp;
-        }
 
         Map<Integer, Integer> map = new HashMap<>();
-        int idx = 0;
+        int idx = emergency.length;
         for (int n : emergency) {
-            map.put(n, ++idx);
+            map.put(n, idx--);
         }
 
         for (int i = 0; i < answer.length; i++) {
