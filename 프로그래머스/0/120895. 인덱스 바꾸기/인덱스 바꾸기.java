0002bd8f34142e4f9376ff5050aa6f4dbbb71c6d
hy <62362285+misty-janne@users.noreply.github.com>
2024-07-23 23:11:43 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] carr = my_string.toCharArray();
+        char temp = carr[num1];
+        carr[num1] = carr[num2];
+        carr[num2] = temp;
+        return new String(carr);
+    }
+}
