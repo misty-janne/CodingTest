ce3031f12f9864043fed987e1bf6a2e50cf1c739
hy <62362285+misty-janne@users.noreply.github.com>
2024-08-23 14:31:29 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+        return sum / arr.length;
+    }
+}
