7830f5df0e1a6ffd03b0e426217853122ec7056c
hy <62362285+misty-janne@users.noreply.github.com>
2024-09-05 20:24:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 15.78 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String new_id) {
+        new_id = new_id.toLowerCase().replaceAll("[^a-z0-9-_.]","").replaceAll("\\.{2,}", ".").replaceAll("^\\.|\\.$", "");
+        if (new_id.isEmpty()) new_id = "a";
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0,15).replaceAll("\\.$","");
+        }
+        while (new_id.length() < 3) {
+            new_id += new_id.charAt(new_id.length()-1);
+        }
+        return new_id;
+    }
+}
