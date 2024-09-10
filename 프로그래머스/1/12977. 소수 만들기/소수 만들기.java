34fd871309b2c8856c6ec23e2ebff3b67f367a4e
hy <62362285+misty-janne@users.noreply.github.com>
2024-09-10 22:23:08 +0900

    [level 1] Title: 소수 만들기, Time: 12.78 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    answer = isPrime(nums[i]+nums[j]+nums[k]) ? answer+1 : answer;
+                }
+            }
+        }
+        return answer;
+    }
+    public boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
